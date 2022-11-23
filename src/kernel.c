61134fc259d5628f49204659d29c4aacb675f309
random_cube <lolyou331@yandex.com>
2022-11-23 01:50:26 +0100

    Paging implemented fully

--- src/kernel.c
+++ src/kernel.c
@@ -93,6 +93,20 @@ void kernel_main()
     //setup paging
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
     
+    //Switch to kernel paging chunk
+    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+
+    char* ptr = kzalloc(4096);   
+    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT |PAGING_IS_WRITEABLE);
+
+    char* ptr2 = (char*) 0x1000;
+    ptr2[0] = 'A';
+    ptr2[1] = 'B';
+    print(ptr2); 
+
+    //enable paging
+    enable_paging();
+
     enable_interrupts(); //enable the system interrupts
         
 }
--- src/memory/paging/paging.asm
+++ src/memory/paging/paging.asm
@@ -5,6 +5,7 @@ section .asm
 global paging_load_directory
 global enable_paging
 
+;load the paging directory with new empty chunks
 paging_load_directory:
     push ebp
     mov ebp, esp
@@ -13,6 +14,7 @@ paging_load_directory:
     pop ebp
     ret
 
+;enable paging in the system
 enable_paging:
     push ebp
     mov ebp, esp

982084d0f1e323a3deca451c40e38373d29f442a
random_cube <lolyou331@yandex.com>
2022-12-31 23:09:48 +0100

    Implementing VFS fopen function

--- src/fs/fat/fat16.c
+++ src/fs/fat/fat16.c
@@ -251,13 +251,16 @@ int fat16_resolve(struct disk* disk)
     struct fat_private* fat_private = kzalloc(sizeof(struct fat_private));
     fat16_init_private(disk, fat_private);
 
+    disk->fs_private = fat_private;
+    disk->filesystem = &fat16_fs;
+
     struct disk_stream* stream = diskstreamer_new(disk->id);
     if (!stream)
     {
         res = -ENOMEM;
         goto out;
     }
-    if (diskstreamer_read(stream, &fat_private->header, sizeof(fat_private->header) != PRACTICEOS_ALL_OK))
+    if (diskstreamer_read(stream, &fat_private->header, sizeof(fat_private->header)) != PRACTICEOS_ALL_OK)
     {
         res = -EIO;
         goto out;
@@ -275,8 +278,7 @@ int fat16_resolve(struct disk* disk)
         goto out;
     }
 
-    disk->fs_private = fat_private;
-    disk->filesystem = &fat16_fs;
+
     
 out:
     if (stream)

9587cd4cbcfe0944c4cae45f9446de76f6feb51f
random_cube <lolyou331@yandex.com>
2023-01-04 02:32:49 +0100

    Changing our kernel segment and data descriptors to be written in C

--- src/kernel.c
+++ src/kernel.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "idt/idt.h"
+#include "memory/memory.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
 #include "disk/disk.h"
@@ -9,6 +10,8 @@
 #include "string/string.h"
 #include "disk/streamer.h"
 #include "fs/file.h"
+#include "gdt/gdt.h"
+#include "config.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -82,11 +85,24 @@ void panic(const char* msg)
     while (1) { }
 }
 
+struct gdt gdt_real[PRACTICEOS_TOTAL_GDT_SEGMENTS];
+struct gdt_structured gdt_structured[PRACTICEOS_TOTAL_GDT_SEGMENTS] = {
+    {.base = 0x00, .limit = 0x00,       .type = 0x00},          // NULL segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0x9a},          //Kernel code segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0x92}           // Kernel data segment
+};
+
 void kernel_main()
 {
     // init the terminal
     terminal_initialize(); 
     print("Hello world\n Hello");
+    
+    memset(gdt_real, 0x00, sizeof(gdt_real));
+    gdt_structured_to_gdt(gdt_real, gdt_structured, PRACTICEOS_TOTAL_GDT_SEGMENTS);
+
+    //Load the GDT
+    gdt_load(gdt_real, sizeof(gdt_real));
     
     //initialize heap
     kheap_init(); 

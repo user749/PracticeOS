8cefc05b924c067cbcefd74df8463ca589a71c94
random_cube <lolyou331@yandex.com>
2023-01-13 02:45:26 +0100

    Reading the task's stack

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -6,8 +6,11 @@ global _start
 
 _start:
 
-    mov eax, 0
+    push 20
+    push 30
+    mov eax, 0 ; command 0 SUM
     int 0x80
-    
+    add esp, 8
 
+    
     jmp $
--- src/task/task.c
+++ src/task/task.c
@@ -183,6 +183,14 @@ int task_page()
     return 0;
 }
 
+int task_page_task(struct task* task)
+{
+    user_registers();
+    paging_switch(task->page_directory);
+    return 0;
+}
+
+
 void task_run_first_ever_task()
 {
     if (!current_task)
@@ -213,3 +221,36 @@ int task_init(struct task* task, struct process* process)
 
     return 0;
 }
+
+void* task_get_stack_item(struct task* task, int index)
+{
+    void* result = 0;
+
+    uint32_t* sp_ptr = (uint32_t*) task->registers.esp;
+
+    // Switch to the given task page
+    task_page_task(task); 
+
+    result = (void*) sp_ptr[index];
+
+    // Switch back to the kernel page
+    kernel_page();
+
+    return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

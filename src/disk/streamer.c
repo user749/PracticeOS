f120610b6cebc78b49735e0db17ca48fa1b770bd
random_cube <lolyou331@yandex.com>
2023-01-21 02:26:38 +0100

    Revising our stream reader

--- src/disk/streamer.c
+++ src/disk/streamer.c
@@ -2,6 +2,8 @@
 #include "memory/heap/kheap.h"
 #include "config.h"
 
+#include <stdbool.h>
+
 struct disk_stream* diskstreamer_new(int disk_id)
 {
     struct disk* disk = disk_get(disk_id);
@@ -26,16 +28,24 @@ int diskstreamer_read(struct disk_stream* stream, void* out, int total)
 {   
     int sector = stream->pos / PRACTICEOS_SECTOR_SIZE;
     int offset = stream->pos % PRACTICEOS_SECTOR_SIZE;
+    
+    int total_to_read = total;
+
+    bool overflow = (offset+total_to_read) >= PRACTICEOS_SECTOR_SIZE;
     char buf[PRACTICEOS_SECTOR_SIZE];
 
+    if (overflow)
+    {
+        total_to_read -= (offset + total_to_read) - PRACTICEOS_SECTOR_SIZE;
+    }
+    
     int res = disk_read_block(stream->disk, sector, 1, buf);
     if (res < 0)
     {
         goto out;
     }
 
-    int total_to_read = total > PRACTICEOS_SECTOR_SIZE ? PRACTICEOS_SECTOR_SIZE : total;
-
+    
     for (int i = 0; i < total_to_read; i++)
     {
         *(char*) out++ = buf[offset+i];
@@ -44,10 +54,10 @@ int diskstreamer_read(struct disk_stream* stream, void* out, int total)
     
     //adjust the stream
     stream->pos += total_to_read;
-    if (total > PRACTICEOS_SECTOR_SIZE)
+    if (overflow)
     {
         //read past the first sector - by recursion
-        res = diskstreamer_read(stream, out, total - PRACTICEOS_SECTOR_SIZE);
+        res = diskstreamer_read(stream, out, total - total_to_read);
     }
     
     out:

3b3c2212d5f626e03b2a609d02655b39d42b2d1c
random_cube <lolyou331@yandex.com>
2023-01-27 20:24:50 +0100

    Implementing the print function in stdlib

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,4 +1,9 @@
+#include "practiceos.h"
+
 int main(int argc, char** argv)
 {
+    print("hello how are you!\n");
+    while (1){}
+    
     return 0;
 }
---
+++ programs/stdlib/src/practiceos.h
@@ -0,0 +1,6 @@
+#ifndef PRACTICEOS_H
+#define PRACTICEOS_H
+
+void print(const char* message);
+
+#endif

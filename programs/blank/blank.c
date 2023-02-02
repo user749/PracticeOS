f984586103dd84aeef6ef2e3b9ad0472228f3629
random_cube <lolyou331@yandex.com>
2023-02-02 12:14:12 +0100

    Creating a shell

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -5,20 +5,7 @@
 int main(int argc, char** argv)
 {
     printf("My age is %i\n", 44);
-    print("hello how are you!\n");
-
-    print(itoa(2332));
-
-    putchar('z');
-
-    void* ptr = malloc(512);
-    free(ptr);
-
-    char buf[1024];
-    practiceos_terminal_readline(buf, sizeof(buf), true);
-
-    print(buf);
-
+ 
 
     while (1)
     {
---
+++ programs/shell/src/shell.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "practiceos.h"
+
+int main(int argc, char** argv)
+{
+    print("PracticeOS V1.0.0\n");
+
+    while (1)
+    {
+        printf("> ");
+        char buf[1024];
+        practiceos_terminal_readline(buf, sizeof(buf), true);
+        print("\n");
+            
+    }
+
+    return 0;
+    
+}
